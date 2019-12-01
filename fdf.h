fe2134bde1624353b7948058d901698558669f17
sleng17 <sammert17@yandex.ru>
2019-12-01 19:07:04 +0300

    8

--- fdf.h
+++ fdf.h
@@ -10,7 +10,7 @@
 # define WIDTH	1800
 # define HEIGHT	1000
 
-# define ESC		65307
+# define ESC	65307
 
 # define MINUS	45
 # define PLUS	61
@@ -35,31 +35,31 @@
 
 #else
 
-# define WIDTH	4000
-# define HEIGHT	3000
+# define WIDTH	2500
+# define HEIGHT	1300
 
-# define ESC		53
+# define ESC	53
 
-# define MINUS	45
-# define PLUS	61
+# define MINUS	27
+# define PLUS	24
 
 # define SPACE	49
 
-# define W		119
-# define S		115
-# define A		97
-# define D		100
+# define W		13
+# define S		1
+# define A		0
+# define D		2
 
-# define Q		113
-# define E		101
+# define Q		12
+# define E		14
 
 # define UP		126
 # define DOWN	125
 # define LEFT	123
 # define RIGHT	124
 
-# define I		105
-# define O		111
+# define I		34
+# define O		31
 
 #endif
 
